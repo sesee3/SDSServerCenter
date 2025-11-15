680aadf6a85bfae75a547838be77b2ac79aa2880
Giuseppe Raffa <77052701+sesee3@users.noreply.github.com>
2025-11-15 18:35:17 +0100

    Added some more configuration directives for server port and url

--- Sources/Portal/EntryPoint.swift
+++ Sources/Portal/EntryPoint.swift
@@ -18,6 +18,9 @@ enum Entrypoint {
         // let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
         // app.logger.debug("Tried to install SwiftNIO's EventLoopGroup as Swift's global concurrency executor", metadata: ["success": .stringConvertible(executorTakeoverSuccess)])
         
+        app.http.server.configuration.hostname = "0.0.0.0"
+        app.http.server.configuration.port = 8080
+        
         do {
             try await configure(app)
             try await app.execute()
